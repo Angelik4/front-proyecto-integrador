be66bf7ebea6d2322b82030cb8443c69677c416d
Gabriela <gabrielahdez00@gmail.com>
2024-02-27 19:24:30 -0500

    Adelanto Detalles

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,43 +1,33 @@
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { StateContext } from '../Components/utils/StateProvider';
+import ImageGallery from 'react-image-gallery';
+import "../css/Detalle.css"
 
 
 const Detail = () => {
   const [state] = useContext(StateContext);
   const { products } = state;
-  const params = useParams()
-  const detalle = products.find(detalle => detalle.id === parseInt(params.id))
-  return (
-    <>
-     <main>
-      <h1>Detalle de cada persona</h1>
-      <div className='container-table'>
-      <h2>id {params.id}</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>Nombre:</td>
-            <td>{detalle?.nombre}</td>
-          </tr>
-          <tr>
-            <td>Email:</td>
-            <td>{detalle?.imagen}</td>
-          </tr>
-          <tr>
-            <td>Teléfono:</td>
-            <td>{detalle?.descripcion}</td>
-          </tr>
-          <tr>
-            <td>Sitio web:</td>
-            <td>{detalle?.calificacion}</td>
-          </tr>
-        </tbody>
-      </table>
-      </div>
-    </main>
-    </>
-  )
-}
+  const params = useParams();
+  const detalle = products.find(detalle => detalle.id === parseInt(params.id));
 
-export default Detail
+
+  return (
+    <div className="main-container">
+      <div className="container-title">
+        <h2>DESCUBRE NUESTROS</h2>
+        <h3>{detalle?.nombre}</h3>
+        <div className="text-container">
+        <h4>{detalle?.categoria}</h4>
+        <p>{detalle?.descripcion}</p>
+        <p> <ul>{detalle?.servicios.map((servicio, index) => (
+            <li key={index}>{servicio}</li>
+            ))}
+            </ul></p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Detail;
