dc317117d5afdb2bd95cf4dfc3d81bc318ea4bf2
BethCarvajal87 <ebreitkh@gmail.com>
2024-03-08 09:56:27 -0500

    se actualiza el login para redireccionar a la home una vez la sesion sea correcta, se agrega mensajes de error en los datos del login

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,46 +1,52 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import logoCoworking from '../images/logo-coworking.webp';
-import { faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import '../css/Navbar.css';
+import Avatar from '@mui/material/Avatar';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import Icon from '@mui/material/Icon';
+
+
 
 
 const Navbar = () => {
+  const { state } = useLocation();
+
+  console.log("Estado" + state);
+
   const [mostrarLista, setMostrarLista] = useState(false);
   const [iconoMenu, setIconoMenu] = useState(true);
-  
+
   const toggleLista = () => {
     setMostrarLista(!mostrarLista);
-  }; 
+  };
   const cambiarIcono = () => {
     setIconoMenu(!iconoMenu);
   };
   return (
-    <section className='ct_container-header'>
-        <div className='ct_header'>
+    <header className='ct_container-header'>
+      <div className='ct_header'>
         <Link to="/"><img src={logoCoworking} alt="logo Coworking Now" /></Link>
-        <button className='iconBurgerMob' onClick={cambiarIcono}> {iconoMenu ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faXmark} /> }</button>
-          <nav className={iconoMenu ? '' : 'active'}>
-              <Link to="/">INICIO</Link>
-              <Link to="/question">¿QUIENES SOMOS?</Link>
-              <div className='ct_containerDrop'>
-                <li onClick={toggleLista}>ESPACIOS <span><FontAwesomeIcon icon={faChevronDown} /></span></li>
-                {mostrarLista && (<div className='ct_dropdown'>
-                  <Link to="/espacios-pet-friendly">ESPACIOS PET FRIENDLY</Link>
-                  <Link to="/espacios-coworking">ESPACIOS COWORKING</Link>
-                  <Link to="/oficinas-virtuales">OFICINAS VIRTUALES</Link>
-                  <Link to="/salas-vip">SALAS VIP</Link>
-                </div>)}
-              </div>
-              <Link to="/galeria">GALERÍA</Link>
-              <Link className='createAccount' to="/register">Crear cuenta</Link>
-              <Link className='btnLogin' to="/login"><FontAwesomeIcon icon={faUser} />Iniciar sesión</Link>
-          </nav>
+        <button className='iconBurgerMob' onClick={cambiarIcono}> {iconoMenu ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faXmark} />}</button>
+        <nav className={iconoMenu ? '' : 'active'}>
+
+          <Link className='createAccount' to="/register">Crear cuenta</Link>
+          <Link className='btnLogin' to="/Login"><FontAwesomeIcon icon={faUser} />Iniciar sesión</Link>
+        </nav>
+
+        <nav>
+          <IconButton color="inherit" aria-label="menu" className='customIcon'>
+            <MenuIcon />
+          </IconButton>
+          <Avatar sx={{ bgcolor: '#F2994A' }}>AN</Avatar>
+        </nav>
       </div>
-    </section>
+    </header>
   )
 }
 
--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,63 +1,126 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 import "../css/Login.css"
 import logoLogin from '../images/Group16.png';
+import Swal from 'sweetalert2';
 
-  function Login() {
-    const [showpwd, setshowpwd] = useState(false);
-    const [email, setEmail] = useState('');
-    const [isValidEmail, setIsValidEmail] = useState(false);
+function Login() {
+  const navigate = useNavigate();
+  const [showpwd, setshowpwd] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   
-    const handleEmailChange = (event) => {
-      const newEmail = event.target.value;
-      setEmail(newEmail);
-      const isValid = validateEmail(newEmail);
-      setIsValidEmail(isValid);
-    };
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    const isValid = validateEmail(newEmail);
+    setIsValidEmail(isValid);
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  };
   
-    const validateEmail = (email) => {
-      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return regex.test(email);
-    };
-    
+
+  const handleLogin = () => {
+    if (email && password && isValidEmail) {
+      setIsAuthenticated(true);      
+      navigate('/home', {
+        replace:true,
+        state:{
+          logged: true,
+          email,
+        },
+      });
+    } else {
+      if (!email) {
+        showErrorAlert('Correo electrónico no ingresado', 'Por favor, ingrese su correo electrónico');
+      } else if (!password) {
+        showErrorAlert('Contraseña no ingresada', 'Por favor, ingrese su contraseña');
+      } else if (!isValidEmail) {
+        showErrorAlert('Correo electrónico no válido', 'Por favor, ingrese un correo electrónico válido');
+      } else {
+        showErrorAlert('Error de autenticación', 'Por favor, complete todos los campos');
+      }
+    }
+  };
+
+  const showErrorAlert = (title, text, iconColor = '#F2994A') => {
+    Swal.fire({
+      icon: 'warning',
+      title: title,
+      text: text,
+      customClass: {
+        container: 'swal2-container-custom', 
+        popup: 'swal2-popup-custom', 
+        title: 'swal2-title-custom', 
+        confirmButton: 'swal2-confirm-custom', 
+        icon: 'swal2-icon-custom', 
+      },
+      showCancelButton: false,
+      confirmButtonColor: iconColor,
+    });
+  };
+  
+
+
   return (
     <div className="contenedor_login">
       <div className="imagen-logo">
-        <img src= {logoLogin} alt="Logo" />
+        <img src={logoLogin} alt="Logo" />
       </div>
       <div className="contenedor_login-title">
-      <h2>Inicio de sesión</h2>
-      <p>Por favor ingrese su usuario y contraseña</p>
+        <h2>Inicio de sesión</h2>
+        <p>Por favor ingrese su usuario y contraseña</p>
       </div>
       <form action="#">
         <div className="campo-formulario">
           <label for="email">Correo electrónico:</label>
-          <input type="email" 
-                  id="email" 
-                  name="email" 
-                  value={email}
-                  placeholder="Esteban_schiller@gmail.com"
-                  onChange={handleEmailChange} /></div>
+          <input type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Esteban_schiller@gmail.com"
+            onChange={handleEmailChange} 
+            required            
+            />
+            </div>
         <div className={`campo-formulario ${isValidEmail ? 'campo-valido' : 'campo-invalido'}`}>
         </div>
-          <div className="campo-formulario">
+        <div className="campo-formulario">
           <label for="contrasena">Contraseña:</label>
-            <input type={showpwd ? "text" : "password"} id="contrasena" name="contrasena" placeholder="**************" />
-            <div className='Icon' onClick={()=>setshowpwd(!showpwd)} >
-            {showpwd ? <span className='icon-eye'><FontAwesomeIcon icon={faEye}/></span> :
-            <span className='icon-eye'><FontAwesomeIcon icon={faEyeSlash}/></span>
+          <input type={showpwd ? "text" : "password"}
+            id="contrasena"
+            name="contrasena"
+            placeholder="**************"
+            onChange={handlePasswordChange}
+            required            
+          />          
+          <div className='Icon' onClick={() => setshowpwd(!showpwd)} >
+            {showpwd ? <span className='icon-eye'><FontAwesomeIcon icon={faEye} /></span> :
+              <span className='icon-eye'><FontAwesomeIcon icon={faEyeSlash} /></span>
             }
-            </div> 
+          </div>
         </div>
         <div className="contenedor_login-recordar-contrasena">
           <input type="checkbox" id="recordar-contrasena" name="recordar-contrasena" />
           <label for="recordar-contrasena">Recordar contraseña</label>
         </div>
-        <button type="submit">Iniciar sesión</button>
-        <a className= 'olvidoContrasena' href= '#'>¿Olvidó su contraseña?</a>
-        <p> ¿No tienes una cuenta? <a href="#"> Crear cuenta</a> </p> 
+        <button type="button" onClick={handleLogin}>Iniciar sesión</button>
+        <a className='olvidoContrasena' href='#'>¿Olvidó su contraseña?</a>
+        <p> ¿No tienes una cuenta? <a href="#"> Crear cuenta</a> </p>
       </form>
     </div>
   );
