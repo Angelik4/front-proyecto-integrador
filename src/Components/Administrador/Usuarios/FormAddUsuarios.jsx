8b4bcd68a9429f5b046116a04dee2b4c8a7ae7ea
BethCarvajal87 <ebreitkh@gmail.com>
2024-03-16 15:01:10 -0500

    Se crear el registrar y listar de usuario

---
+++ src/Components/Administrador/Usuarios/FormAddUsuarios.jsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import sendRequest from "../../utils/SendRequest";
+
+const FormAddUsuarios = ({ isOpen, onRequestClose }) => {
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [rolId, setRolId] = useState(""); 
+    const [estado, setEstado] = useState("activo"); 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            
+            const nombreCompleto = nombre + " " + apellido;
+            
+            const rolesResponse = await sendRequest("GET", "http://localhost:8081/rol/listar");
+            const rolEncontrado = rolesResponse.find((rol) => rol.nombre === rolId);
+
+            if (!rolEncontrado) {
+                console.error("Rol no encontrado.");
+                return;
+            }
+
+            const response = await sendRequest("POST", "http://localhost:8081/usuario/registrar", {
+                nombre: nombreCompleto,
+                correo: correo,
+                contrasena: contrasena,
+                idTipoIdentificacion: 1,
+                numeroIdentificacion: 7854236,
+                estado: estado === "activo" ? 1 : 0,
+                idRol: rolEncontrado.id,
+            });
+            console.log("Respuesta del servidor:", response);
+            onRequestClose();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const customStyles = {
+        content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+        },
+    };
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="Agregar Categoría"
+            ariaHideApp={false}
+            style={customStyles}
+        >
+            <div className="modal-container">
+                <button onClick={onRequestClose} className="btn-cerrar">
+                    <FontAwesomeIcon icon={faTimes} />
+                </button>
+                <h2>Agregar Usuario</h2>
+                <form className="modal-form" onSubmit={handleSubmit}>
+                    <div className="form-row">
+                        <div className="form-group">
+                            <label htmlFor="nombre">Nombre</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                placeholder="Nombre"
+                                value={nombre}
+                                onChange={(e) => setNombre(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="apellido">Apellido</label>
+                            <input
+                                type="text"
+                                id="apellido"
+                                placeholder="Apellido"
+                                value={apellido}
+                                onChange={(e) => setApellido(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="form-group">
+                            <label htmlFor="correo">Correo</label>
+                            <input
+                                type="text"
+                                id="correo"
+                                placeholder="Correo"
+                                value={correo}
+                                onChange={(e) => setCorreo(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="contrasena">Contraseña</label>
+                            <input
+                                type="password"
+                                id="contrasena"
+                                placeholder="Contraseña"
+                                value={contrasena}
+                                onChange={(e) => setContrasena(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="form-group">
+                            <label htmlFor="rol">Rol</label>
+                            <select
+                                id="rol"
+                                value={rolId}
+                                onChange={(e) => setRolId(e.target.value)}
+                            >
+                                <option value="">Seleccionar Rol</option>
+                                <option value="Administrador">Administrador</option>
+                                <option value="Usuario Registrado">Usuario Registrado</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="estado">Estado</label>
+                            <select
+                                id="estado"
+                                value={estado}
+                                onChange={(e) => setEstado(e.target.value)}
+                            >
+                                <option value="activo">Activo</option>
+                                <option value="inactivo">Inactivo</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn-guardar">
+                        Guardar
+                    </button>
+                </form>
+            </div>
+        </Modal>
+    );
+};
+
+export default FormAddUsuarios;
--- src/Components/Administrador/Usuarios/Usuario.jsx
+++ src/Components/Administrador/Usuarios/Usuario.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
-import '../../../css/Panel.css'
+import '../../../css/Panel.css';
+import FormAddUsuarios from '../Usuarios/FormAddUsuarios';
+import sendRequest from '../../utils/SendRequest';
+import FormDelete from '../Salas/FormDelete';
 
 const Usuario = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [usuarios, setUsuarios] = useState([]);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const listarUsuarios = async () => {
+    try {
+      const response = await sendRequest('GET', 'http://localhost:8081/usuario/listar');
+      console.log('Usuarios obtenidos:', response);
+      if (response && response.length > 0) {
+        const usuariosFormateados = response.map((usuario) => {
+          const nombreCompleto = usuario.nombre.split(' ');
+          const nombre = nombreCompleto[0] || '';
+          const apellido = nombreCompleto.slice(1).join(' ') || ''; 
+          return {
+            id: usuario.id || '',
+            nombre: nombre || '',
+            apellido: apellido || '',
+            correo: usuario.correo || '',
+            contrasena: '******',
+            rol: usuario.idRol ? usuario.idRol.nombre : '',
+            estado: usuario.estado === 1 ? 'Activo' : 'Inactivo',
+          };
+        });
+        setUsuarios(usuariosFormateados);
+      } else {
+        console.error('La respuesta no contiene datos válidos:', response);
+      }
+    } catch (error) {
+      console.error('Error al obtener los usuarios:', error);
+    }
+  };
+  
+
+  useEffect(() => {
+    listarUsuarios();
+  }, []);
+
   return (
     <div className="Ct-Tabla">
       <div className="buscador-container">
@@ -11,11 +60,14 @@ const Usuario = () => {
           <FontAwesomeIcon icon={faSearch} style={{ color: '#333', marginRight: '5px' }} />
           <input type="text" placeholder="Buscar por Nombre/ID" />
         </div>
-        <button className="agregar-usuario">
+        <button className="agregar-usuario" onClick={openModal}>
           <FontAwesomeIcon icon={faUserPlus} style={{ color: '#fff', marginRight: '5px' }} />
           Agregar Usuario
         </button>
       </div>
+      {/* <button className="btn-listar" onClick={listarUsuarios}>
+        Listar Usuarios
+      </button> */}
       <table>
         <thead>
           <tr>
@@ -30,60 +82,28 @@ const Usuario = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Gabriela</td>
-            <td>Hernández</td>
-            <td>Gjfjsofbeo@gmail.com</td>
-            <td>**************</td>
-            <td>Administrador</td>
-            <td>Activo</td>
-            <td>
-              <button>Editar</button>
-              <button>Eliminar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Esmeralda</td>
-            <td>Benitez</td>
-            <td>Efjsofbeo@gmail.com</td>
-            <td>**************</td>
-            <td>Usuario</td>
-            <td>Activo</td>
-            <td>
-              <button>Editar</button>
-              <button>Eliminar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Angelica</td>
-            <td>Urrego</td>
-            <td>Bfjsofbeo@gmail.com</td>
-            <td>**************</td>
-            <td>Administrador</td>
-            <td>Activo</td>
-            <td>
-              <button>Editar</button>
-              <button>Eliminar</button>
-            </td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Beatriz</td>
-            <td>Carvajal</td>
-            <td>UUfjsofbeo@gmail.com</td>
-            <td>**************</td>
-            <td>Usuario</td>
-            <td>Inactivo</td>
-            <td>
-              <button>Editar</button>
-              <button>Eliminar</button>
-            </td>
-          </tr>
+          {usuarios.map((usuario) => (
+            <tr key={usuario.id}>
+              <td>{usuario.id}</td>
+              <td>{usuario.nombre}</td>
+              <td>{usuario.apellido}</td> 
+              <td>{usuario.correo}</td>
+              <td>{usuario.contrasena}</td>
+              <td>{usuario.rol}</td>
+              <td>{usuario.estado}</td>
+              <td>
+                <button className="editar-usuario" onClick={() => openModal('edit', usuario)}>
+                  Editar
+                </button>
+                <button className="eliminar-usuario" onClick={() => openModal('delete', usuario)}>
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+      <FormAddUsuarios isOpen={modalIsOpen} onRequestClose={closeModal} />
     </div>
   );
 };
