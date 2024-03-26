e58b7fa7c5df9a2fcb8ae4d61b6bdbbc0fe63ecd
murrego <murrego@grupodot.com>
2024-03-26 15:40:25 -0500

    Ajustes sobre login con validación de token

--- src/Components/utils/SendRequest.jsx
+++ src/Components/utils/SendRequest.jsx
@@ -2,29 +2,37 @@ import axios from "axios";
 
 const sendRequest = async (method, endpoint, data = null) => {
   try {
+    let token = localStorage.getItem('token'); // Obtener el token del localStorage
+
+    let config = {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    // Si hay un token, agregarlo a los encabezados de la solicitud
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
     let response;
     switch (method) {
       case "GET":
-        response = await axios.get(endpoint);
+        response = await axios.get(endpoint, config);
         break;
       case "POST":
-        response = await axios.post(endpoint, data, {
-          headers: {
-            'Access-Control-Allow-Headers': 'POST, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-            'Content-Type': 'application/json',
-        }
-        });
+        response = await axios.post(endpoint, data, config);
         break;
       case "PUT":
-        response = await axios.put(endpoint, data);
+        response = await axios.put(endpoint, data, config);
         break;
       case "DELETE":
-        response = await axios.delete(endpoint);
+        response = await axios.delete(endpoint, config);
         break;
       default:
         throw new Error("Método HTTP no válido");
     }
-    return response.data; 
+    return response.data;
   } catch (error) {
     throw new Error(`Error al enviar la solicitud ${method} a ${endpoint}: ${error}`);
   }
