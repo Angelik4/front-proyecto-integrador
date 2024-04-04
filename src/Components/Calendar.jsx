d7a8122d0da6b80b429e5c4441cada3a89bad912
Gabriela <gabrielahdez00@gmail.com>
2024-04-04 18:10:21 -0500

    Actualizaciones calendario

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import es from 'date-fns/locale/es';
-import { setHours, setMinutes } from 'date-fns';
+import { setHours, setMinutes, format } from 'date-fns';
 import sendRequest from "../Components/utils/SendRequest"
 registerLocale('es', es);
 
@@ -13,25 +13,37 @@ const Calendar = () => {
   
   const fetchAvailableRooms = async () => {
     try {
-      // Convertir fecha y hora de inicio al huso horario local
-      const startDateTime = new Date(startDate.getTime() - (startDate.getTimezoneOffset() * 60000));
-      const formattedStartDate = startDateTime.toISOString();
+      // Formatear fecha y hora de inicio
+      const formattedStartDate = format(startDate, "yyyy-MM-dd'T'HH:mm");
 
-      // Convertir fecha y hora de finalización al huso horario local
-      const endDateTime = new Date(endDate.getTime() - (endDate.getTimezoneOffset() * 60000));
-      const formattedEndDate = endDateTime.toISOString();
+      // Formatear fecha y hora de finalización
+      const formattedEndDate = format(endDate, "yyyy-MM-dd'T'HH:mm");
+
+      console.log("Fecha de inicio:", formattedStartDate);
+      console.log("Fecha de finalización:", formattedEndDate);
 
       const endpoint = `http://localhost:8081/reservaespacio/salasDisponibles/${formattedStartDate}/${formattedEndDate}`;
       const response = await fetch(endpoint, {
         method: 'GET'
       });
+
       if (!response.ok) {
-        throw new Error("Error al obtener las salas disponibles");
+        throw new Error("Error al obtener las salas disponibles: " + response.statusText);
       }
+
       const data = await response.json();
-      setAvailableTimes(data.salasDisponibles);
+
+      console.log("Data:", data); // Agregar impresión de data para depurar
+
+      if (!data || !data.salasDisponibles) {
+        throw new Error("No se recibieron datos de las salas disponibles");
+      }
+
+      console.log("Salas disponibles:", data.salasDisponibles);
+      setAvailableTimes(data || []);
     } catch (error) {
       console.error("Error al buscar salas disponibles:", error);
+      setAvailableTimes([]); // Establecer availableTimes como un arreglo vacío en caso de error
     }
   };
 
@@ -47,7 +59,7 @@ const Calendar = () => {
         startDate={startDate}
         selectsStart
         timeIntervals={60}
-        dateFormat="yyyy/MM/dd h aa"
+        dateFormat="yyyy/MM/dd HH:mm"
         showTimeSelect
         withPortal
         className="auto-width"
@@ -61,7 +73,7 @@ const Calendar = () => {
         endDate={endDate}
         selectsEnd
         timeIntervals={60}
-        dateFormat="yyyy/MM/dd h aa"
+        dateFormat="yyyy/MM/dd HH:mm"
         showTimeSelect
         withPortal
         className="auto-width"
