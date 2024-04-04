3d30a2882638f88f4649b21ea093247ff1179954
murrego <murrego@grupodot.com>
2024-04-04 11:31:49 -0500

    Ajuste pagina de detalles

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,85 +1,69 @@
-import { Link, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWifi, faWind, faMugSaucer, faGlassWater, faCookieBite, faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
-import "../css/Detalle.css";
-import ButtonReservar from "../Components/ButtonReservar";
-import axios from "axios";
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import ButtonReservar from '../Components/ButtonReservar';
+import sendRequest from '../Components/utils/SendRequest';
 
 const Detail = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [images, setImages] = useState([]);
 
-useEffect(() => {
-  axios.get(`http://localhost:8081/sala/listar/${id}`)
-  .then(response => setProduct(response.data))
-  .catch(error => console.log(error))
-}, [id])
-  
- const [images, setImages] = useState([]);
- 
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await sendRequest('GET', `http://localhost:8081/sala/busqueda/${id}`);
+        console.log('Respuesta de la solicitud:', response); // Agregar console para ver la respuesta
+        setProduct(response);
+      } catch (error) {
+        console.error('Error al obtener el producto:', error);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
   useEffect(() => {
     if (product) {
-      const formattedImages = product.galery?.map((url) => ({
+      const formattedImages = product.imagenes?.map((url) => ({
         original: url,
         thumbnail: url,
       }));
       setImages(formattedImages);
     }
-  }, [product]);  
-
-  const iconMapping = {
-    wifi: faWifi,
-    'aire acondicionado': faWind,
-    café: faMugSaucer,
-    agua: faGlassWater,
-    'snack free': faCookieBite,
-  };
-
-  function renderIcon(servicio) {
-    const icon = iconMapping[servicio.toLowerCase()];
-    return icon ? <FontAwesomeIcon className="icon-service" icon={icon} /> : null;
-  }
+  }, [product]);
 
   return (
-   <>
-   {
-    product &&  <div className="main-container">
-    <Link to="/" className="btn_atras">
-     <FontAwesomeIcon className="iconArrow" icon={faAngleRight} /> Atrás
-   </Link>
-   <div className="container-title">
-     <h2>DESCUBRE NUESTROS</h2>
-     <h3>{product.nombre}</h3>
-     <div className="content-details">
-       <div className="text-container">
-         <h4>{product.categoria}</h4>
-         <p>{product.descripcion}</p>
-         <ul>
-           {product.servicios?.map((servicio, index) => (
-             <li key={index}>
-               {renderIcon(servicio)}{servicio}
-             </li>
-           ))}
-         </ul>
-         <ButtonReservar />
-       </div> 
-        <div>
-         <ImageGallery
-           items={images}
-           showPlayButton={false}
-           showNav={false}
-           showBullets={true}
-           thumbnailPosition="right"
-         />
-       </div> 
-     </div>
+    <div className="main-container">
+      <Link to="/" className="btn_atras">
+        <FontAwesomeIcon className="iconArrow" icon={faAngleRight} /> Atrás
+      </Link>
+      {product && (
+        <div className="container-title">
+          <h2>DESCUBRE NUESTROS</h2>
+          <h3>{product.nombre}</h3>
+          <div className="content-details">
+            <div className="text-container">
+              <h4>{product.categoria}</h4>
+              <p>{product.descripcion}</p>
+              <ButtonReservar />
+            </div>
+            <div>
+              <ImageGallery
+                items={images}
+                showPlayButton={false}
+                showNav={false}
+                showBullets={true}
+                thumbnailPosition="right"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
- </div>
-   }
-   </>
   );
 };
 
